600eef976f6a4c1bc235f4da37a5b3ddbb868c87
Wandrille <wandrille@winch4.ai>
2025-01-28 18:11:59 +0100

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+//import type { Metadata } from "next";
+//import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+/*
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
+*/
 export const metadata = {
   title: "Mon Calculateur de Temps",
   description: "Estimation de temps de course avec la formule de Riegel",
