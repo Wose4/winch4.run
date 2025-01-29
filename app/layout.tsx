1ea314ffa3caf007da315f5f7f0b6e8b1e4ecf51
Wandrille <wandrille@winch4.ai>
2025-01-29 08:47:17 +0100

    mini update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
-//import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-/*
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-*/
 export const metadata = {
   title: "Mon Calculateur de Temps",
   description: "Estimation de temps de course avec la formule de Riegel",
@@ -19,9 +7,7 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="fr">
       <body>
